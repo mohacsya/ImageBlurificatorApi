fe743492f7ed5a87376e4e991cb569e425bf8179
mohacsya <36229408+mohacsya@users.noreply.github.com>
2025-10-17 23:18:38 +0200

    build targets harmonized along projects

--- ImageBlurificatorApi/Services/Implementations/OpenCvImageProcessor.cs
+++ ImageBlurificatorApi/Services/Implementations/OpenCvImageProcessor.cs
@@ -1,13 +1,17 @@
 ﻿using ImageBlurificatorApi.Models;
 using ImageBlurificatorApi.Services.Interfaces;
+using GaussianBlurCore;
 
 namespace ImageBlurificatorApi.Services.Implementations
 {
     public class OpenCvImageProcessor : IImageProcessor
     {
+
         public Task<byte[]> ProcessAsync(string imageBase64, EncodingType encoding, CancellationToken token)
         {
-            throw new NotImplementedException();
+            var processor = new GaussianBlurCoreProcessor();
+            return null;
         }
+
     }
 }
